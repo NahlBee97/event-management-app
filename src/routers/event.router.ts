4f1fcdbe74dda70da00e5ea9b7dd0c592f98306c
ethutami uta <ethutami@gmail.com>
2025-04-20 18:54:19 +0800

    create api event detail

--- src/routers/event.router.ts
+++ src/routers/event.router.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { GetAllCategoriesController } from "../controllers/categories.controller";
 import { GetAllEventController } from "../controllers/event.controller";
+import { GetEventDetailByIdController } from "../controllers/event_detail.controller";
 
 const router = Router();
 
 router.get('/', GetAllEventController)
+
+router.get('/detail/:id', GetEventDetailByIdController)
+
 router.get('/categories', GetAllCategoriesController);
 
 
