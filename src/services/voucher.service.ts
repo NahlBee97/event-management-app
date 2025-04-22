5a1f68f85cfcc01422ba69809debc38872ff863d
ethutami uta <ethutami@gmail.com>
2025-04-22 17:45:24 +0800

    update enpoint "get voucher by eventid" and create enpoint "get voucher by userid"

--- src/services/voucher.service.ts
+++ src/services/voucher.service.ts
@@ -2,9 +2,25 @@ import prisma from "../lib/prisma";
 
 export async function GetVoucherByEventIdServices(params: string) {
     try {
-        const voucher = await prisma.vouchers.findFirst({
+        const voucher = await prisma?.event_vouchers.findFirst({
             where: {
                 event_id: Number(params)
+            },
+        })
+
+        if (!voucher) throw new Error('Voucher not found')
+
+        return voucher
+    } catch (err) {
+        throw err;
+    }
+}
+
+export async function GetVoucherByUserIdServices(params: string) {
+    try {
+        const voucher = await prisma?.user_vouchers.findFirst({
+            where: {
+                user_id: Number(params)
             },
         })
 
