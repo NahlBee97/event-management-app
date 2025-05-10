27cd2eb5391105b9d1895ffe9ae81a654b1f8353
ethutami uta <ethutami@gmail.com>
2025-05-10 15:53:14 +0800

    change date response to local zone

--- src/services/search_event.service.ts
+++ src/services/search_event.service.ts
@@ -1,25 +1,32 @@
 import { ISearchEvent } from "../interfaces/event.interface";
 import prisma from "../lib/prisma";
 
+function toLocalISOString(date: Date, timeZone: string): string {
+    const localString = date.toLocaleString("sv-SE", {
+        timeZone,
+        hour12: false,
+    });
+    return localString.replace(" ", "T") + ".000";
+}
+
 export async function SearchEventService(params: ISearchEvent) {
     try {
         const { category_id, start_date, end_date, min_price, max_price, organizer_id } = params
         const filters: any = {};
+        const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
         if (category_id) {
             filters.category_id = Number(category_id);
         }
 
         if (start_date) {
-            filters.start_date = {
-                gte: new Date(start_date),
-            };
+            const localStartDate = new Date(start_date);
+            filters.start_date = { gte: new Date(toLocalISOString(localStartDate, userTimeZone)) };
         }
 
         if (end_date) {
-            filters.end_date = {
-                lte: new Date(end_date),
-            };
+            const localEndDate = new Date(end_date);
+            filters.end_date = { lte: new Date(toLocalISOString(localEndDate, userTimeZone)) };
         }
 
         if (min_price || max_price) {
@@ -44,7 +51,15 @@ export async function SearchEventService(params: ISearchEvent) {
 
         if (!events) throw new Error(`Event not found`)
 
-        return events
+        const eventsWithLocalTime = events.map(event => {
+            return {
+                ...event,
+                start_date: toLocalISOString(new Date(event.start_date), userTimeZone),
+                end_date: toLocalISOString(new Date(event.end_date), userTimeZone),
+            };
+        });
+
+        return eventsWithLocalTime
     } catch (error) {
         throw error
     }
