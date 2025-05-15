04506a20edeedb6c4ee8f4781118a552d5898ac5
ethutami uta <ethutami@gmail.com>
2025-05-15 18:32:40 +0800

    create get all voucher by organizer

--- src/controllers/voucher.controller.ts
+++ src/controllers/voucher.controller.ts
@@ -1,49 +1,49 @@
 import { NextFunction, Request, Response } from "express";
-import { CreateVoucherService, DeleteVoucherByIdService, EditVoucherByIdService, GetVoucherByEventIdServices, GetVoucherByUserIdServices } from "../services/voucher.service";
+import { CreateVoucherService, DeleteVoucherByIdService, EditVoucherByIdService, GetAllVoucherByOrganizerIdService, GetVoucherByEventIdServices, GetVoucherByUserIdServices } from "../services/voucher.service";
 
 export async function GetVoucherByEventIdController(req: Request, res: Response, next: NextFunction) {
-    try {
-        const eventId = req?.params?.id
+  try {
+    const eventId = req?.params?.id
 
-        const voucher = await GetVoucherByEventIdServices(eventId);
+    const voucher = await GetVoucherByEventIdServices(eventId);
 
-        res.status(200).send({
-            success: true,
-            message: `Get voucher with id ${eventId} is success`,
-            data: voucher
-        })
-    } catch (err) {
-        next(err)
-    }
+    res.status(200).send({
+      success: true,
+      message: `Get voucher with id ${eventId} is success`,
+      data: voucher
+    })
+  } catch (err) {
+    next(err)
+  }
 }
 
 export async function GetVoucherByUserIdController(req: Request, res: Response, next: NextFunction) {
-    try {
-        const userId = req?.params?.id
-        const voucher = await GetVoucherByUserIdServices(userId);
+  try {
+    const userId = req?.params?.id
+    const voucher = await GetVoucherByUserIdServices(userId);
 
-        res.status(200).send({
-            success: true,
-            message: `Get voucher with user id ${userId} is success`,
-            data: voucher
-        })
-    } catch (err) {
-        next(err);
-    }
+    res.status(200).send({
+      success: true,
+      message: `Get voucher with user id ${userId} is success`,
+      data: voucher
+    })
+  } catch (err) {
+    next(err);
+  }
 }
 
 export async function CreateVoucherController(req: Request, res: Response, next: NextFunction) {
-    try {
-        const voucher = await CreateVoucherService(req.body); 
+  try {
+    const voucher = await CreateVoucherService(req.body);
 
-        res.status(200).send({
-          success: true,
-          message: `create new voucher for event id ${req.body.event_id} is success`,
-          data: voucher,
-        });
-    } catch (err) {
-        next(err);
-    }
+    res.status(200).send({
+      success: true,
+      message: `create new voucher for event id ${req.body.event_id} is success`,
+      data: voucher,
+    });
+  } catch (err) {
+    next(err);
+  }
 }
 
 export async function EditVoucherByIdController(
@@ -82,5 +82,21 @@ export async function DeleteVoucherByIdController(
     });
   } catch (err) {
     next(err);
+  }
+}
+
+export async function GetAllVoucherByOrganizerIdController(req: Request, res: Response, next: NextFunction) {
+  try {
+    const id = Number(req?.params?.id)
+    const vouchers = await GetAllVoucherByOrganizerIdService(id)
+
+    res.status(200).send({
+      success: true,
+      message: `Get voucher is success`,
+      data: vouchers
+    })
+
+  } catch (error) {
+    next(error)
   }
 }
