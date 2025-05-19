2e827b33b7b39bffb6a4595f845bcdd343dc7fe8
Nahalil <nahalilmuchtar2@gmail.com>
2025-05-19 23:58:21 +0800

    fix update profile

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -16,7 +16,5 @@ export const loginSchema = z.object({
 export const updateSchema = z.object({
   email: z.string().email("Invalid Email Format").trim(),
   first_name: z.string().nonempty("First name is required"),
-  last_name: z.string().nonempty("Last name is required"),
-  role: z.string().nonempty("role is required"),
-  password: z.string().nonempty("Password is required"),
+  last_name: z.string().nonempty("Last name is required")
 });
